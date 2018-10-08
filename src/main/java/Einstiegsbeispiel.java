7aca3af6607425cd660a0b42239c0423cea11dc1
criedl1 <christoph.riedl1@gmail.com>
2018-10-08 15:21:00 +0200

    Added method isPalindrome

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Einstiegsbeispiel {
+
+    public static void main(String[] args) {
+
+        Scanner S1 = new Scanner(System.in);
+        String Puffer = S1.next();
+
+        System.out.println(isPalindrome(Puffer));
+
+
+    }
+
+    public static int isPalindrome (String Puffer){
+        int pali = 1;
+        char [] arr1 = Puffer.toCharArray();
+
+        for (int i = 0 ; i <arr1.length/2 ; i++) {
+            if (arr1 [i] != arr1 [(arr1.length-1)-i])
+                pali = 0;
+        }
+        return pali;
+
+    }
+
+
 }
