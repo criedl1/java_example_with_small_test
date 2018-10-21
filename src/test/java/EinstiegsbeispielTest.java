aa300022ed1e175dab38821d65ab72f3a486a52b
criedl1 <christoph.riedl1@gmail.com>
2018-10-21 14:09:22 +0200

    isPalindrome finished + first Test finished

--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -1,2 +1,17 @@
-public class EinstiegsbeispielTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EinstiegsbeispielTest {
+
+    @Test
+    public void testPalindrome(){
+        Einstiegsbeispiel positivetest = new Einstiegsbeispiel();
+
+        int x = positivetest.isPalindrome("anna");
+        assertEquals(1,x);
+
+
+
+    }
 }
